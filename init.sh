b8bdfca08fc668ad8f9e53cfcb5f6ad5d1a70825
liyachao <liyachao.knight@bytedance.com>
2019-01-01 12:32:48 +0800

    feature:complete init.sh

--- init.sh
+++ init.sh
@@ -2,85 +2,24 @@
 
 echo "begin init project~~"
 
-./gradlew clean :knight-transform:uploadArchives --stacktrace 2>&1| tee log.txt
-buildResult=`grep "BUILD FAILED" log.txt`
-if [[ ${buildResult} =~ "BUILD FAILED" ]]
-then
-    echo "please check your project"
-    exit 1
-fi
-rm -f log.txt
+list="knight-transform knight-byteK knight-doubleCheck-plugin knight-shrinkR-plugin knight-config-plugin knight-doubleCheck-library"
 
-#knight-byteK
-sed -i "" "s/\/\/k//g" knight-byteK/build.gradle
-./gradlew clean :knight-byteK:uploadArchives --stacktrace 2>&1| tee log.txt
-buildResult=`grep "BUILD FAILED" log.txt`
-if [[ ${buildResult} =~ "BUILD FAILED" ]]
-then
-    echo "please check your project"
-    exit 1
-fi
-rm -f log.txt
-
-
-#knight-doubleCheck-plugin
-sed -i "" "s/\/\/k//g" knight-doubleCheck-plugin/build.gradle
-./gradlew clean :knight-doubleCheck-plugin:uploadArchives --stacktrace 2>&1| tee log.txt
-buildResult=`grep "BUILD FAILED" log.txt`
-if [[ ${buildResult} =~ "BUILD FAILED" ]]
-then
-    echo "please check your project"
-    exit 1
-fi
-rm -f log.txt
-
-#knight-shrinkR-plugin
-sed -i "" "s/\/\/k//g" knight-shrinkR-plugin/build.gradle
-./gradlew clean :knight-shrinkR-plugin:uploadArchives --stacktrace 2>&1| tee log.txt
-buildResult=`grep "BUILD FAILED" log.txt`
-if [[ ${buildResult} =~ "BUILD FAILED" ]]
-then
-    echo "please check your project"
-    exit 1
-fi
-rm -f log.txt
-
-#knight-config-plugin
-sed -i "" "s/\/\/k//g" knight-config-plugin/build.gradle
-./gradlew clean :knight-config-plugin:uploadArchives --stacktrace 2>&1| tee log.txt
-buildResult=`grep "BUILD FAILED" log.txt`
-if [[ ${buildResult} =~ "BUILD FAILED" ]]
-then
-    echo "please check your project"
-    exit 1
-fi
-rm -f log.txt
-
-#knight-doubleCheck-library
-sed -i "" "s/\/\/k//g" knight-doubleCheck-library/build.gradle
-./gradlew clean :knight-doubleCheck-library:uploadArchives --stacktrace 2>&1| tee log.txt
-buildResult=`grep "BUILD FAILED" log.txt`
-if [[ ${buildResult} =~ "BUILD FAILED" ]]
-then
-    echo "please check your project"
-    exit 1
-fi
-rm -f log.txt
-
-#library
-./gradlew clean :library:uploadArchives --stacktrace 2>&1| tee log.txt
-buildResult=`grep "BUILD FAILED" log.txt`
-if [[ ${buildResult} =~ "BUILD FAILED" ]]
-then
-    echo "please check your project"
-    exit 1
-fi
-rm -f log.txt
+for state in ${list}
+do
+    echo "this word is $state"
+    sed -i "" "s/\/\/k//g" ${state}/build.gradle
+    ./gradlew clean :${state}:uploadArchives --stacktrace 2>&1| tee log.txt
+    buildResult=`grep "BUILD FAILED" log.txt`
+    if [[ ${buildResult} =~ "BUILD FAILED" ]]
+    then
+        echo "please check your project"
+        exit 1
+    fi
+    rm -f log.txt
+done
 
 #project build.gradle
 sed -i "" "s/\/\/k//g" build.gradle
 
 #app build.gradle
 sed -i "" "s/\/\/k//g" app/build.gradle
-
-./gradlew build

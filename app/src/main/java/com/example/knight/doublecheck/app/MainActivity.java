80a514b1282248467196575888682457be050b86
liyachao <296777513@qq.com>
2019-01-15 20:42:43 +0800

    delete demo file

--- app/src/main/java/com/example/knight/doublecheck/app/MainActivity.java
+++ app/src/main/java/com/example/knight/doublecheck/app/MainActivity.java
@@ -11,11 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.library.MyButton;
-import com.example.library.MyView;
-import com.knight.component.library.ServiceManager;
 import com.knight.doublecheck.library.DoubleCheck;
-import com.knight.module1.Module1Activity;
-import com.knight.module1.Module1Service;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     int i = 0;
@@ -31,12 +27,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "no double check: " + (++i), Toast.LENGTH_LONG).show();
-                Module1Service service = ServiceManager.getIns().getService(Module1Service.class);
-                if (service != null) {
-                    service.startModule1Activity(MainActivity.this);
-                }
-
-//                com.knight.module1.BuildConfig
             }
         });
         Button button1 = findViewById(R.id.text2);
--- module.module1/src/main/java/com/knight/module1/Module1Service.kt
+++
@@ -1,8 +0,0 @@
-package com.knight.module1
-
-import android.content.Context
-
-interface Module1Service {
-
-    fun startModule1Activity(context: Context)
-}

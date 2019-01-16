b30594aa291f01e923a1d10f4182009d07941e6f
liyachao <296777513@qq.com>
2019-01-16 21:13:21 +0800

    add a temp file , this file is to introduce how to write ASMcode

---
+++ knight-component-library/src/main/kotlin/com/knight/component/library/ServiceManagerJava.java
@@ -0,0 +1,107 @@
+package com.knight.component.library;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.res.Configuration;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zhounl on 2017/11/15.
+ */
+
+public class ServiceManagerJava {
+
+    private List<Application> moduleApplications = new ArrayList<>();
+
+    private Map<Class, Class> routersMap = new HashMap<>();
+
+    private Map<Class, Object> routerInstanceMap = new HashMap<>();
+
+    private ServiceManagerJava() { }
+
+    public void attachBaseContext(Context context) {
+        for (Application app : moduleApplications) {
+            try {
+                // invoke each application's attachBaseContext
+                Method attachBaseContext = ContextWrapper.class.getDeclaredMethod("attachBaseContext", Context.class);
+                attachBaseContext.setAccessible(true);
+                attachBaseContext.invoke(app, context);
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void onCreate() {
+        for (Application app : moduleApplications) {
+            app.onCreate();
+        }
+    }
+
+    public void onConfigurationChanged(Configuration configuration) {
+        for (Application app : moduleApplications) {
+            app.onConfigurationChanged(configuration);
+        }
+    }
+
+    public void onLowMemory() {
+        for (Application app : moduleApplications) {
+            app.onLowMemory();
+        }
+    }
+
+    public void onTerminate() {
+        for (Application app : moduleApplications) {
+            app.onTerminate();
+        }
+
+    }
+
+    public void onTrimMemory(int level) {
+        for (Application app : moduleApplications) {
+            app.onTrimMemory(level);
+        }
+    }
+
+    public static synchronized <T> T service(Class<T> routerType) {
+        T requiredRouter = null;
+        if (!get().routerInstanceMap.containsKey(routerType)) {
+            try {
+                requiredRouter = (T) get().routersMap.get(routerType).newInstance();
+                get().routerInstanceMap.put(routerType, requiredRouter);
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        } else {
+            requiredRouter = (T) get().routerInstanceMap.get(routerType);
+        }
+        return requiredRouter;
+    }
+
+    public List<Application> moduleApplications() {
+        return moduleApplications;
+    }
+
+    public Map<Class, Class> routersMap() {
+        return routersMap;
+    }
+
+    public static AppJoint get() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    static class SingletonHolder {
+        static AppJoint INSTANCE = new AppJoint();
+    }
+}
